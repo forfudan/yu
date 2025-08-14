ca3d77b2ac39e2672ec4703cdfc289b66373b5d7
ZHU Yuhao 朱宇浩 <dr.yuhao.zhu@outlook.com>
2025-08-14 12:42:28 +0200

    Update hint

--- components/search/FetchSearch.vue
+++ components/search/FetchSearch.vue
@@ -58,7 +58,7 @@ watch(userInput, (newValue) => {
     <div>
         <!-- Always show the input field -->
         <label class="input input-bordered bg-gray-100 dark:bg-slate-800 flex items-center gap-2 mt-2">
-            <input v-model="userInput" type="text" class="grow" placeholder="输入汉字，查看拆分和编码" />
+            <input v-model="userInput" type="text" class="grow" placeholder="輸入漢字查詢拆分和編碼" />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70">
                 <path fill-rule="evenodd"
                     d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
